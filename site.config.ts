fe3558ee73350273af0b9d50987d82ca77a03f04
SayMyWebname <105113328+SayMyWebname@users.noreply.github.com>
2023-03-08 19:40:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '06c1675c37a04f4e8e0bbd57a12cd124',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '广州艾威科技发展有限公司',
+  domain: 'https://www.notion.so/nauhoag/Web-06c1675c37a04f4e8e0bbd57a12cd124',
+  author: '高欢',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
